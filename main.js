0587defc831d65a75f083fdc84e85c9a2f569ae0
denys1953 <den25640@gmail.com>
2021-04-17 16:22:54 +0300

    master

---
+++ main.js
@@ -0,0 +1,264 @@
+let field = document.getElementById('field-type')
+let score = document.getElementById('score')
+let best = document.getElementById('best')
+let elemCount, count2, count = 0
+let array = []
+let array2 = []
+let subarray = []
+let subarray2 = []
+let size = 4
+let sum = 0
+let filter = []
+
+
+for (let i = 0; i < 16; i++) {
+   let elem = document.createElement('div')
+   field.append(elem);
+   elem.className = 'elem'
+   elem.innerHTML = " ";
+}
+
+let elems = document.getElementsByClassName('elem')
+
+function generateNums(a) {
+   setTimeout(() => {
+      count = 0
+      count2 = 0
+      for (let i = 0; i < elems.length; i++) {
+         if (elems[i].innerHTML == '&nbsp;') {
+            count2++
+         }
+      }
+      if (count2 > 0) {
+         while (count < a) {
+            let randNum = randomInteger(0, 15)
+            if (elems[randNum].innerHTML !== '&nbsp;') {
+               continue
+            } else {
+               elems[randNum].innerHTML = 2
+               // elems[randNum].style.backgroundColor = '#eee4da'
+               scoreCount()
+            }
+            count++
+         }
+      }
+   }, 200)
+   setColor()
+}
+
+generateNums(2)
+
+function setColor() {
+   for (let i = 0; i < elems.length; i++) {
+      if (elems[i].innerHTML == 2) elems[i].style.backgroundColor = "#eee4da"
+      else if (elems[i].innerHTML == 4) elems[i].style.backgroundColor = "#eee1c9";
+      else if (elems[i].innerHTML == 8) elems[i].style.backgroundColor = "#f3b27a"
+      else if (elems[i].innerHTML == 16) elems[i].style.backgroundColor = "#f69664"
+      else if (elems[i].innerHTML == 32) elems[i].style.backgroundColor = "#f67c60"
+      else if (elems[i].innerHTML == 64) elems[i].style.backgroundColor = "#f65e3b"
+      else if (elems[i].innerHTML == 128) elems[i].style.backgroundColor = "#edcf73"
+      else if (elems[i].innerHTML == 256) elems[i].style.backgroundColor = "#edcc62"
+      else if (elems[i].innerHTML == 512) elems[i].style.backgroundColor = "#edc850"
+      else if (elems[i].innerHTML == 1024) elems[i].style.backgroundColor = "#edc53f"
+      else if (elems[i].innerHTML == 2048) elems[i].style.backgroundColor = "#edc22d"
+      else {
+         elems[i].style.backgroundColor = '#cdc1b4'
+      }
+   }
+   for (let i = 0; i < elems.length; i++) {
+      if (elems[i].innerHTML == 2 || elems[i].innerHTML == 4) {
+         elems[i].style.color = '#776e65'
+      } else {
+         elems[i].style.color = '#f9f6f2'
+      }
+   }
+}
+
+setColor()
+
+function mainLeft() {
+   let sss
+   for (let i = 0; i < subarray.length; i++) {
+      sss = subarray[i].filter(num => num.length < 5)
+      while (sss.length < 4) sss.push('&nbsp;')
+      filter.push(sss)  
+      if (filter[i][0] === filter[i][1] && filter[i][0] !== '&nbsp;') {
+         filter[i][0] *= 2; filter[i][1] = '&nbsp;'
+      } else if (filter[i][1] === filter[i][2] && filter[i][1] !== '&nbsp;') {
+         filter[i][1] *= 2; filter[i][2] = '&nbsp;'
+      } else if (filter[i][2] === filter[i][3] && filter[i][2] !== '&nbsp;') {
+         filter[i][2] *= 2; filter[i][3] = '&nbsp;'
+      }
+   }
+}
+function mainRight() {
+   let sss
+   for (let i = 0; i < subarray.length; i++) {
+      sss = subarray[i].filter(num => num.length < 5)
+      while (sss.length < 4) sss.unshift('&nbsp;')
+      filter.push(sss)  
+      if (filter[i][3] === filter[i][2] && filter[i][3] !== '&nbsp;') {
+         filter[i][3] *= 2; filter[i][2] = '&nbsp;'
+      } else if (filter[i][2] === filter[i][1] && filter[i][2] !== '&nbsp;') {
+         filter[i][2] *= 2; filter[i][1] = '&nbsp;'
+      } else if (filter[i][1] === filter[i][0] && filter[i][1] !== '&nbsp;') {
+         filter[i][1] *= 2; filter[i][0] = '&nbsp;'
+      }
+   }
+}
+function mainUp() {
+   let sss
+   for (let i = 0; i < subarray2.length; i++) {
+      sss = subarray2[i].filter(num => num.length < 5)
+      while (sss.length < 4) sss.push('&nbsp;')
+      filter.push(sss)
+      if (sss[0] === sss[1] && sss[0] !== '&nbsp;') {
+         sss[0] *= 2; sss[1] = '&nbsp;'
+      } else if (sss[1] === sss[2] && sss[1] !== '&nbsp;') {
+         sss[1] *= 2; sss[2] = '&nbsp;'
+      } else if (sss[2] === sss[3] && sss[2] !== '&nbsp;') {
+         sss[2] *= 2; sss[3] = '&nbsp;'
+      }
+   }
+}
+function mainDown() {
+   let sss
+   for (let i = 0; i < subarray2.length; i++) {
+      sss = subarray2[i].filter(num => num.length < 5)
+      while (sss.length < 4) sss.unshift('&nbsp;')
+      filter.push(sss)
+      if (sss[3] === sss[2] && sss[3] !== '&nbsp;') {
+         sss[3] *= 2; sss[2] = '&nbsp;'
+      } else if (sss[2] === sss[1] && sss[2] !== '&nbsp;') {
+         sss[2] *= 2; sss[1] = '&nbsp;'
+      } else if (sss[1] === sss[0] && sss[1] !== '&nbsp;') {
+         sss[1] *= 2; sss[0] = '&nbsp;'
+      }
+   }
+}
+
+function randomInteger(min, max) {
+   let rand = min - 0.5 + Math.random() * (max - min + 1);
+   return Math.round(rand);
+}
+function  reClearArr(){
+   array.splice(0, array.length)
+   filter.splice(0, filter.length)
+   for (let i = 0; i < elems.length; i++) {
+      array.push(elems[i].innerHTML)
+   }
+   
+   for (let i = 0; i <Math.ceil(array.length/size); i++){
+       subarray[i] = array.slice((i*size), (i*size) + size);
+   }
+   
+}
+function clearField() {
+   for (let i = 0; i < elems.length; i++) {
+      elems[i].innerHTML = "&nbsp;"
+   }
+}
+
+function reDrawField() {
+   let reserveArr = filter[0].concat(filter[1], filter[2], filter[3])
+   for (let i = 0; i < elems.length; i++) {
+      elems[i].innerHTML = reserveArr[i]
+   }
+}
+
+function hReDrawField() {
+   let reserveArr = subarray[0].concat(subarray[1], subarray[2], subarray[3])
+   for (let i = 0; i < elems.length; i++) {
+      elems[i].innerHTML = reserveArr[i]
+   }
+}
+function verticalReClearArr() {
+   array2.splice(0, array2.length)
+   reClearArr()
+   let j = 0;
+   while (j < 4) {
+      for (let i = 0; i < subarray.length; i++) {
+         array2.push(subarray[i][j])
+      }
+      j++
+   }
+   for (let i = 0; i <Math.ceil(array2.length/size); i++){
+      subarray2[i] = array2.slice((i*size), (i*size) + size);
+   }
+}
+function horisontalReClearArr() {
+   array.splice(0, array.length)
+   subarray.splice(0, subarray.length)
+   let j = 0;
+   while (j < 4) {
+      for (let i = 0; i < filter.length; i++) {
+         array.push(filter[i][j])
+      }
+      j++
+   }
+   for (let i = 0; i <Math.ceil(array.length/size); i++){
+      subarray[i] = array.slice((i*size), (i*size) + size);
+   }
+}
+function scoreCount() {
+   sum = 0
+   for (let i = 0; i < elems.length; i++) {
+      if (elems[i].innerHTML !== '&nbsp;') {
+         sum+=parseInt(elems[i].innerHTML)
+         score.innerHTML = "Score: " + sum
+         if (sum > localStorage.getItem('best')) {
+            localStorage.setItem('best', sum);
+         }
+         best.innerHTML = 'Best: ' + localStorage.getItem('best')
+      }
+   }
+   setColor()
+}
+
+scoreCount()
+
+reClearArr()
+
+document.addEventListener('keydown', (e) => {
+   switch (e.keyCode) {
+      case 37:
+         buttonLeft()
+         generateNums(1)
+         break
+      case 38:
+         buttonUp()
+         generateNums(1)
+         break
+      case 39:
+         buttonRight()
+         generateNums(1)
+         break
+      case 40:
+         buttonDown()
+         generateNums(1)
+         break
+   }
+})
+
+function buttonLeft() {
+   reClearArr()
+   mainLeft(); mainLeft()
+   reDrawField()
+}
+function buttonRight() {
+   reClearArr()
+   mainRight(); mainRight()
+   reDrawField()
+}
+function buttonUp() {
+   verticalReClearArr()
+   mainUp(); 
+   horisontalReClearArr()
+   hReDrawField()
+}
+function buttonDown() {
+   verticalReClearArr()
+   mainDown();
+   horisontalReClearArr()
+   hReDrawField()
+}
