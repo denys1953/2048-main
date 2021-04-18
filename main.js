4203f13119049ccc5248ff9e5acbdf81f276d773
denys1953 <den25640@gmail.com>
2021-04-18 12:25:52 +0300

    master

--- main.js
+++ main.js
@@ -2,11 +2,6 @@ let field = document.getElementById('field-type')
 let score = document.getElementById('score')
 let best = document.getElementById('best')
 let container = document.getElementsByClassName('container')
-let btn = document.getElementsByClassName('btn')
-btn[0].addEventListener('click', buttonLeft)
-btn[1].addEventListener('click', buttonDown)
-btn[2].addEventListener('click', buttonUp)
-btn[3].addEventListener('click', buttonRight)
 let elemCount, count2, count = 0
 let array = []
 let array2 = []
@@ -259,6 +254,70 @@ document.addEventListener('keydown', (e) => {
       case 40:
          buttonDown()
          break
+   }
+})
+
+function swipedetect(el, callback){
+  
+   var touchsurface = el,
+   swipedir,
+   dist,
+   startX,
+   startY,
+   distX,
+   distY,
+   threshold = 150, 
+   restraint = 100, 
+   allowedTime = 300, 
+   elapsedTime,
+   startTime,
+   handleswipe = callback || function(swipedir){}
+ 
+   touchsurface.addEventListener('touchstart', function(e){
+       var touchobj = e.changedTouches[0]
+       swipedir = 'none'
+       dist = 0
+       startX = touchobj.pageX
+       startY = touchobj.pageY
+       startTime = new Date().getTime() 
+       e.preventDefault()
+   }, false)
+ 
+   touchsurface.addEventListener('touchmove', function(e){
+       e.preventDefault() 
+   }, false)
+ 
+   touchsurface.addEventListener('touchend', function(e){
+       var touchobj = e.changedTouches[0]
+       distX = touchobj.pageX - startX 
+       distY = touchobj.pageY - startY 
+       elapsedTime = new Date().getTime() - startTime 
+       if (elapsedTime <= allowedTime){ 
+           if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint){ 
+               swipedir = (distX < 0)? 'left' : 'right'
+           }
+           else if (Math.abs(distY) >= threshold && Math.abs(distX) <= restraint){ 
+               swipedir = (distY < 0)? 'up' : 'down' 
+           }
+       }
+       handleswipe(swipedir)
+       e.preventDefault()
+   }, false)
+}
+
+
+var el = document.getElementById('wrapper')
+var inner = document.getElementById('inner')
+var hidetimer = null
+swipedetect(el, function(swipedir){
+   if (swipedir == 'left'){
+      buttonLeft()
+   } else if (swipedir == 'right') {
+      buttonRight()
+   } else if (swipedir == 'up') {
+      buttonUp()
+   } else if (swipedir == 'down') {
+      buttonDown()
    }
 })
 
